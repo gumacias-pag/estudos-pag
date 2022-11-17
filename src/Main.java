717278a4ac2b3ab22b760397b1cd33e3540200ca
grozolin <grozolin@uolinc.com>
2022-11-17 14:13:50 -0300

    Calculadora com interface grafica

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 //este é um comentario em linha
 
 /* Tipos primitivos de variaveis
@@ -19,13 +21,11 @@ import java.awt.*;
  * && -> somente verdadeiro se os dois valores logicos forem verdadeiros
  * || -> se uns dos valores é verdadeiro retorna verdadeiro;
  *
- * JOptionPane.showInputDialog("Mensagem que vau aparecer"); -> Telinha para o usuario digitar
+ * JOptionPane.showInputDialog("Mensagem que vai aparecer"); -> Telinha para o usuario digitar
  * JOptionPane -> A telinha implementada
  *
  *  */
 public class Main {
-    String msg = "msg1";
-
     public static void main(String[] args) {
         Main main = new Main();
 
@@ -34,53 +34,82 @@ public class Main {
         tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);////quando fechar a tela o programa termina
 
         JPanel painel = new JPanel();
-        painel.setLayout(new FlowLayout());
+        painel.setLayout(new FlowLayout()); //organiza o compponente (FlowLayout)
         painel.setSize(300, 400);
 
-        JTextField display = new JTextField("Teste");
-        display.setPreferredSize(new Dimension(290, 100));
-        display.setSize(290,50);
+        JTextArea display = new JTextArea();
+        display.setFont(display.getFont().deriveFont(16f));
+        display.setPreferredSize(new Dimension(290, 100)); //setPreferredSize define o tamanho
+        display.setEditable(false); //para que não possa alterar o texto
 
-        JButton botao1 = new JButton("1");
-        painel.add(display);
-        painel.add(botao1);
+        painel.add(display); //campo de texto
+
+        JButton []botoes = new JButton[10];
+        for (int i = 0; i <= 9; i++){
+            botoes[i] = new JButton(String.valueOf(i));
+            botoes[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent evento) {
+                    String valorDigitado = ((JButton)evento.getSource()).getText();
+                   display.setText(display.getText()+valorDigitado); //valor digitado mantém + outro valor digitado
+                }
+            });
+            painel.add(botoes[i]);
+        }
+
+        JButton botaoSimbolo;
+
+        String valorBotoes[] = {"+", "-", "/", "x"};
+
+        //length? até o comprimento do vetor?
+        for(int i = 0; i < valorBotoes.length; i++){ //percorrendo o vetor e add os botoes
+            botaoSimbolo = new JButton(valorBotoes[i]);
+            botaoSimbolo.addActionListener(new ActionListener() { // adicionando o comportamento do botão
+                @Override
+                public void actionPerformed(ActionEvent evento) {
+                    String valorDigitado = ((JButton)evento.getSource()).getText();
+                    display.setText(display.getText()+valorDigitado); //valor digitado mantém + outro valor digitado
+                }
+            });
+            painel.add(botaoSimbolo);
+        }
+
+        botaoSimbolo = new JButton("=");
+        painel.add(botaoSimbolo);
+        botaoSimbolo.addActionListener(new ActionListener() { // adicionando o comportamento do botão
+            @Override
+            public void actionPerformed(ActionEvent evento) {
+                int resultado = main.processaTexto(display.getText());
+                display.setText("" + resultado);
+            }
+        });
+
 
         tela.add(painel);
+
+        Icon icon = new ImageIcon("73858.png");
+
+        JButton botaoLimpa = new JButton("C",icon);
+        painel.add(botaoLimpa);
+
+        botaoLimpa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText("");
+            }
+        });
+
         tela.setVisible(true);
-
-
-        /*int var1, var2;
-
-        var1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor 1: "));
-        var2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor 2: "));
-        double resultado = 0;
-
-        String[] opcoes = {"soma", "subtracao", "divisao", "multiplicacao"};
-        int opcao = JOptionPane.showOptionDialog(null,
-                "Escolha a operação",
-                "Titulo",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.PLAIN_MESSAGE,
-                null,
-                opcoes, null);
-
-        switch (opcao){
-            case 0:
-                resultado = main.soma(var1, var2);
-                break;
-            case 1:
-                resultado = main.subtracao(var1, var2);
-                break;
-            case 2:
-                resultado = main.divisao(var1, var2);
-                break;
-            case 3:
-                resultado = main.multiplicacao(var1, var2);
-                break;
-        }*/
-        //JOptionPane.showMessageDialog(null, resultado);
     }
 
+    public int processaTexto(String texto){
+        String[] valores = texto.split("[+/\\-*]"); // separa o texto recebido do que esta dentro dos parenteses
+        int[] numeros = new int[valores.length];
+        for (int i = 0; i < valores.length; i++){
+        numeros[i] = Integer.parseInt(valores[i]);
+        }
+        return soma(numeros[0],numeros[1]);
+    }
 
     public int soma(int var1, int var2) {
         int resultado = var1 + var2;
---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    String corDaPele;
+    String nome;
+    public Pessoa (String corDaPele){
+        this.corDaPele = corDaPele;
+    }
+    public Pessoa (String corDaPele, String nome){
+        this.corDaPele = corDaPele;
+        this.nome = nome;
+    }
+}
+
+// atributo são as caracteristicas (cabelo, altura, peso)
+// metodo ações (pegar, andar)
